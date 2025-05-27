af8712e00704c1478f63150914923f1708500535
Benício <166818033+benicin@users.noreply.github.com>
2025-05-27 15:58:35 -0300

    Add files via upload

---
+++ benicio-2b/perguntas.js
@@ -0,0 +1,53 @@
+criaCartao(
+    'Harry Potter e as Relíquias da Morte',
+    'Qual é o nome do professor de Defesa Contra as Artes das Trevas que ensina no terceiro livro?    ',
+    'Remus Lupin'
+)
+
+criaCartao(
+    'Harry Potter e a Pedra Filosofal',
+    'Em qual livro Harry descobre que é um famoso bruxo e que seus pais foram assassinados por Voldemort?',
+    'Harry Potter e a Pedra Filosofal'
+)
+
+criaCartao(
+    'Harry Potter e o Cálice de Fogo ',
+    'Qual é o nome do torneio em que Harry participa no quarto livro, que envolve três escolas de magia?',
+    'Torneio Tribruxo'
+)
+
+criaCartao(
+    'Harry Potter e o Enigma do Príncipe ',
+    'Quem é o novo professor de Poções que assume a posição no sexto livro?',
+    'Horace Slughorn'
+)
+
+criaCartao(
+    '?',
+    'Em qual livro Harry e seus amigos formam a "Armada de Dumbledore" para aprender defesa contra as artes das trevas?',
+    'Harry Potter e a Ordem da Fênix'
+)
+
+criaCartao(
+    'Harry Potter e a Câmara Secreta',
+    'Qual é o nome do elfo doméstico que Harry liberta no segundo livro?',
+    'Dobby'
+)
+
+criaCartao(
+    '?',
+    'Em qual livro Harry encontra o Horcrux que é um diário?',
+    'Harry Potter e a Câmara Secreta'
+)
+
+criaCartao(
+    'Harry Potter e o Prisioneiro de Azkaban',
+    'Qual é o nome do feitiço que Harry usa para se defender contra Dementadores no terceiro livro?',
+    'Expecto Patronum'
+)
+
+criaCartao(
+    'Harry Potter e as Relíquias da Morte',
+    'Qual é o nome do último Horcrux que Harry e seus amigos precisam destruir no último livro?',
+    'Nagini'
+)
